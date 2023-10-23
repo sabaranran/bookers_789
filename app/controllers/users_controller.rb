e1bd8301d051038d2eb3815dcb035ddb1b57df21
EC2 Default User <ec2-user@ip-172-31-32-228.ap-northeast-1.compute.internal>
2023-10-23 01:52:45 +0000

    7b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,23 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
+    @today = @books.created_today.count
+    @yesterday = @books.created_yesterday.count
+    @this_week = @books.created_this_week.count
+    @last_week = @books.created_last_week.count
+    
+    if @yesterday == 0
+      @ty = "前日の投稿はなし"
+    else
+      @ty = (@today/@yesterday*100).round
+    end
+    
+    if @last_week == 0
+      @tl = "前週の投稿はなし"
+    else
+      @tl = (@this_week/@last_week*100).round
+    end
+    
   end
 
   def index
