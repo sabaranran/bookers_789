b1d48dc0d78dec07342c1fdefc50d6ae240fc4c1
EC2 Default User <ec2-user@ip-172-31-32-228.ap-northeast-1.compute.internal>
2023-10-25 03:14:26 +0000

    8d

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,6 +7,9 @@ class Book < ApplicationRecord
 	has_many :favorites, dependent: :destroy
 	has_many :book_comments,dependent: :destroy
 	
+	scope :latest, -> {order(created_at: :desc)}
+	scope :star_count, -> {order(star: :desc)}
+	
 	def favorited_by?(user)
 	  favorites.exists?(user_id: user.id)
 	end
